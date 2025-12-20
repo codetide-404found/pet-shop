3e546e3d37399989ad3151d0364ffabf13f37c50
codetide-404 <Isaacdoe1817@gmail.com>
2025-12-20 15:32:48 +0000

    Fix URL routing and allowed hosts

--- backend_shop/urls.py
+++ backend_shop/urls.py
@@ -6,10 +6,10 @@ from pets import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="home"),  # home page from pets app
-    path("", include("pets.urls")),
-  # include all pets routes
+    path("", views.index, name="home"),
+    path("pets/", include("pets.urls")),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
